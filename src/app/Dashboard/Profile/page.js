25f54fd74d5cca3ce1283839cceccc69a554cadf
Muhammad Ali <muhammadali.dev5@gmail.com>
2025-01-28 15:25:51 +0500

    profile fully updated

--- src/app/Dashboard/Profile/page.js
+++ src/app/Dashboard/Profile/page.js
@@ -18,7 +18,8 @@ const Page = () => {
     useravatar: "",
   });
   const [imagePreview, setImagePreview] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(true);
+  const [showConfirmPassword, setConfirmShowPassword] = useState(true);
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
@@ -59,6 +60,7 @@ const Page = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+
     setFormData({ ...formData, [name]: value });
   };
 
@@ -115,21 +117,35 @@ const Page = () => {
       <Header className="min-w-full" />
       <div className="flex gap-4 w-full ">
         <Sidebar />
-        <main className="w-full mt-5 min-h-screen">
+        <main className="w-full mt-5 ">
         <h1 className="text-[#313342] font-medium text-2xl mb-5 underline decoration-[#AEADEB] underline-offset-8 w-full">Profile</h1>
 
           <section className="w-[98%] bg-white">
             <div className="flex flex-col justify-between w-full drop-shadow-custom4 rounded-[10px] ">
-              <div className="text-black  border-b-2 border-[#31334266] py-2 px-6 font-sans font-medium text-2xl">Information</div>
+              <div className="text-black text-center sm:text-start border-b-2 border-[#31334266] py-2 px-6 font-sans font-medium text-2xl">Information</div>
               <div className="bg-transparent  rounded-xl p-8 w-full mx-auto ">
                 {/* <h2 className="text-2xl font-semibold mb-6 text-center">
                   Edit User
                 </h2> */}
-                {loading && <p>Loading...</p>}
-                <form onSubmit={handleSubmit} className="w-full flex flex-col gap-20">
+                {loading?(<p>Loading...</p>):(
+                <form onSubmit={handleSubmit} className="w-full flex flex-col gap-10 sm:gap-20">
                     {/* image */}
                   <div className=" flex justify-center">
-                    <div className=" flex items-center h-[140px] w-[140px] sm:h-[203px] sm:w-[203px] rounded-full border-2 border-black">
+                    <div className=" overflow-hidden relative flex items-center h-[140px] w-[140px] sm:h-[203px] sm:w-[203px] rounded-full border-2 ">
+                    <input
+                        type="file"
+                        className="  z-50 block absolute w-full text-sm text-gray-500 
+                        file:mr-4 file:py-2 file:px-4 file:rounded-full
+                        file:border-0 file:text-sm file:font-semibold
+                        file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 bg-transparent hover:cursor-pointer bottom-0 opacity-0 right-20"
+                        id="useravatar"
+                        name="useravatar"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                      />
+                      <div className="bg-[#00000021]  flex justify-center items-center backdrop-blur-[1px] h-10 sm:h-12  absolute w-full bottom-0 border-[#FFFFFF1A] "> 
+                        <img src="/camera-solid.svg" className=" h-[20px] w-[20px] sm:h-[30px] sm:w-[30px] hover:cursor-pointer" ></img>
+                         </div>
                       {imagePreview ? (
                         <img
                           src={imagePreview}
@@ -147,24 +163,14 @@ const Page = () => {
                     {/* <div className="mt-1 px-3 py-1.5 w-[50%] sm:w-[70%] rounded-md border-gray-400 border focus:outline-none focus:border-indigo-500 text-black">
                     <label className="block">
                       <span className="text-gray-950">Upload file</span>
-                      <input
-                        type="file"
-                        className="block w-full text-sm text-gray-500 
-                        file:mr-4 file:py-2 file:px-4 file:rounded-full
-                        file:border-0 file:text-sm file:font-semibold
-                        file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
-                        id="useravatar"
-                        name="useravatar"
-                        accept="image/*"
-                        onChange={handleImageChange}
-                      />
+                      
                     </label>
                   </div> */}
                   </div>
                   
                   {/* input fields */}
-                  <div className="w-full  flex flex-col gap-10">
-                  <div className="flex gap-3  flex-col md:flex-row">
+                  <div className="w-full  flex flex-col gap-5 sm:gap-10">
+                  <div className="flex gap-6  flex-col md:flex-row">
                     <div className="w-full md:w-[50%]">
                       <label
                         className="block text-gray-700 text-sm font-bold mb-2"
@@ -199,19 +205,30 @@ const Page = () => {
                     </div>
                   </div>
 
-                  <div className="flex gap-3  flex-col md:flex-row">
-                    <div className="w-full md:w-[50%]">
+                  <div className="flex gap-6   flex-col md:flex-row ">
+                    <div className="w-full md:w-[50%] relative">
                       <label htmlFor="password" className="text-gray-950">
                         Password:
                       </label>
+
+                      <div className="relative">
                       <input
-                        type={showPassword ? "text" : "password"}
+                        type={showPassword ? "password":"text" }
                         id="password"
                         name="password"
                         className="w-full px-3 py-2 border  border-[#42506666] rounded-4  focus:outline-none focus:ring focus:ring-indigo-200"
                         value={formData.password}
                         onChange={handleChange}
                       />
+                      <button type="button"
+          onClick={()=>{
+            setShowPassword(!showPassword);
+          }}
+          >
+        
+            <img className="absolute top-[9.665px]  right-3 object-cover object-center"src={`/eyeIcon${showPassword?"On":"Off"}.svg`}></img>
+            </button>
+            </div>
                     </div>
                     <div className="w-full md:w-[50%]">
                       <label
@@ -220,14 +237,24 @@ const Page = () => {
                       >
                         Confirm Password:
                       </label>
+                      <div className="relative">
                       <input
-                        type={showPassword ? "text" : "password"}
+                        type={showConfirmPassword ? "password":"text"}
                         id="confirmpassword"
                         name="confirmpassword"
                         className="w-full px-3 py-2 border  border-[#42506666] rounded-4  focus:outline-none focus:ring focus:ring-indigo-200"
                         value={formData.confirmpassword}
                         onChange={handleChange}
                       />
+                      <button type="button"
+          onClick={()=>{
+            setConfirmShowPassword(!showConfirmPassword);
+          }}
+          >
+        
+            <img className="absolute top-[9.665px]  right-3 object-cover object-center"src={`/eyeIcon${showConfirmPassword?"On":"Off"}.svg`}></img>
+            </button>
+                      </div>
                     </div>
                     {/* <button
                       type="button"
@@ -239,7 +266,10 @@ const Page = () => {
                   </div>
                   <div className=" w-full flex justify-center sm:justify-end ">
                     <div className=" w-full sm:w-auto flex gap-6 flex-col  sm:flex-row ">
-                    <button className="py-1 px-5 w-full sm:w-auto border-[1px] rounded-4 border-[#313342] bg-white text-[#313342] hover:bg-gray-600 hover:text-white focus:ring-4 focus:ring-gray-400 focus:ring-opacity-50 transition-all duration-500"
+                    <button 
+                    type="button"
+                    onClick={() => router.back()}
+                     className="py-1 px-5 w-full sm:w-auto border-[1px] rounded-4 border-[#313342] bg-white text-[#313342] hover:bg-gray-600 hover:text-white focus:ring-4 focus:ring-gray-400 focus:ring-opacity-50 transition-all duration-500"
                     >Cancel</button>
                     <button
                       type="submit"
@@ -251,6 +281,7 @@ const Page = () => {
                   </div>
                   </div>
                 </form>
+                )}
               </div>
             </div>
           </section>
